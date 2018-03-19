37c4a9f70e7912f068336db52ae073c9c269744f
TuanPham303 <phamcongtuan303@gmail.com>
2018-03-19 10:29:45 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const groupmq = require('gulp-group-css-media-queries');
+const bs = require('browser-sync');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const minify = require('gulp-minify-css');
+
+/**
+ * Compile Sass files
+ */
+gulp.task('compile:sass', () =>
+  gulp
+    .src('sass/**/*.scss') // Grab all sass files in sass folder
+    .pipe(plumber()) // Prevent termination on error
+    .pipe(
+      sass({
+        indentType: 'tab',
+        indentWidth: 1,
+        outputStyle: 'expanded' // Expanded so that our CSS is readable
+      })
+    )
+    .on('error', sass.logError)
+    .pipe(
+      postcss([
+        autoprefixer({
+          browsers: ['last 2 versions'],
+          cascade: false
+        })
+      ])
+    )
+    .pipe(groupmq()) // Group media queries!
+    .pipe(gulp.dest('./src/styles/css')) // Output compiled files in the CSS folder
+    .pipe(bs.stream())
+); // Stream to browserSync
+
+/**
+ * Start up browserSync and Watch Sass files for changes
+ */
+gulp.task('watch:sass', ['compile:sass'], () => {
+  bs.init({
+    proxy: 'http://localhost:8888/soberlife', //change the name of the project here
+  });
+
+  gulp.watch('sass/**/*.scss', ['compile:sass']);
+});
+
+gulp.task('js', function(){
+ gulp.src('src/scripts/*.js')
+ .pipe(concat('final-script.js'))
+ .pipe(uglify())
+ .pipe(gulp.dest('build/scripts/')); // the final minified and concatted .js file will be found here
+});
+
+gulp.task('css', function(){
+   gulp.src('src/styles/css/*.css')
+   .pipe(concat('final-styles.css'))
+   .pipe(minify())
+   .pipe(gulp.dest('build/styles/')); // the final minified and concatted .css file will be found here
+});
+
+/**
+ * Default task executed by running `gulp`
+ */
+gulp.task('default', ['watch:sass', 'css', 'js'], function () {
+  // watch for CSS changes
+  gulp.watch('src/styles/css/*.css', function() {
+    // run css upon changes
+    gulp.run('css');
+  });
+  // watch for JS changes
+  gulp.watch('src/scripts/*.js', function() {
+    // run js upon changes
+    gulp.run('js');
+  });
+});

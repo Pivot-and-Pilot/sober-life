3ebbe093a7cdfa8bef7b8ab63b74c008715b67d8
Mijung Lee <mijung.m@gmail.com>
2018-03-20 17:03:37 -0700

    started about page

---
+++ page-about.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package sober-life
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="about" class="site-main">
+      <img src="" alt="" class="about__opening-photo">
+      <div class="about__opening-text"></div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();

91ae0a748832c978fe73db27614fae5eed926c1e
TuanPham303 <phamcongtuan303@gmail.com>
2018-04-10 15:28:09 -0700

    add basic bone

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -47,11 +47,61 @@ jQuery(document).ready(function($){
 
     // get client substance used
     $('div[name="substances-used"]').on('click', function () {
+      $(this).toggleClass('choice-chosen');
       let substancesUsed = $(this).data('value');
-      $(`input[data-name="substances-used-hidden"][value="${substancesUsed}"]`).prop('checked', true);
+      if ( $(this).hasClass('choice-chosen') ) {
+        $(`input[data-name="substances-used-hidden"][value="${substancesUsed}"]`).prop('checked', true);
+      } else {
+        $(`input[data-name="substances-used-hidden"][value="${substancesUsed}"]`).prop('checked', false);
+      }
     })
     $('.other-substances-used').on('change keyup paste', function () {
       $('.other-substances-used-hidden').val($(this).val());
+    })
+
+    // get how drug effect life
+    $('div[name="effect-life"]').on('click', function () {
+      $(this).toggleClass('choice-chosen');
+      let effect = $(this).data('value');
+      if ( $(this).hasClass('choice-chosen') ) {
+        $(`input[data-name="effect-life-hidden"][value="${effect}"]`).prop('checked', true);
+      } else {
+        $(`input[data-name="effect-life-hidden"][value="${effect}"]`).prop('checked', false);
+      }
+    })
+
+    // get client personal info
+    $('.form__phone-number').on('change keyup paste', function () {
+      $('.form__phone-number--hidden').val($(this).val());
+    })
+    $('.form__city').on('change keyup paste', function () {
+      $('.form__city--hidden').val($(this).val());
+    })
+    $('.form__email').on('change keyup paste', function () {
+      $('.form__email--hidden').val($(this).val());
+    })
+    $('.form__state').on('change keyup paste', function () {
+      $('.form__state--hidden').val($(this).val());
+    })
+    $('.form__dob').on('change keyup paste', function () {
+      $('.form__dob--hidden').val($(this).val());
+    })
+    $('input[name="insurance"]').on('click', function () {
+      let answer = $(this).val();
+      $(`input[name="insurance-hidden"][value="${answer}"]`).prop('checked', true);
+    })
+    $('input[name="insurance-company"]').on('change keyup paste', function () {
+      $('input[name="insurance-company-hidden"]').val($(this).val());
+    })
+    $('textarea[name="comments"]').on('change keyup paste', function () {
+      $('textarea[name="comments-hidden"]').val($(this).val());
+    })
+
+  })();
+
+  (function submit () {
+    $('#form__submit').on('click', function () {
+      $('#form__submit--hidden').click();
     })
   })();
   
--- template-parts/content-form.php
+++ template-parts/content-form.php
@@ -1,23 +1,109 @@
 <section id="soberlife-form-wrapper">
-  <div class="form__x-button">X</div>
-  <form action="">
+  <div class="form__relative-layer">
+    <?php
+      if ($_POST) {
+        $clientName = $_POST["client-name-hidden"];
+        $lovedOneName = $_POST["loved-one-name-hidden"];
+        $clientTarget = $_POST["client-target-hidden"];
+        $clientGender = $_POST["client-gender-hidden"];
+        $clientFeeling = $_POST["client-feeling-hidden"];
+        $lovedOneFeeling = $_POST["loved-one-feeling-hidden"];
+        $substancesUsed = array();
+        for ( $count = 1; $count < 10; $count++) {
+          if ($_POST['substances-used-' . $count]) {
+            array_push($substancesUsed, $_POST['substances-used-' . $count]);
+          }
+        }
+        $otherSubstances = $_POST["other-substances-hidden"];
+
+        $effects = array();
+        for ( $c = 1; $c < 3; $c++) {
+          if ($_POST['effect-' . $c]) {
+            array_push($effects, $_POST['effect-' . $c]);
+          }
+        }
+        $phoneNumber = $_POST["phone-number-hidden"];
+        $city = $_POST["city-hidden"];
+        $email = $_POST["email-hidden"];
+        $state = $_POST["state-hidden"];
+        $dob = $_POST["dob-hidden"];
+        $insurance = $_POST["insurance-hidden"];
+        $insuranceCompany = $_POST["insurance-company-hidden"];
+        $comments = $_POST["comments-hidden"];
+
+        $messageBody = "Client name: $clientName \n";
+        if ( $lovedOneName ) {
+          $messageBody .= "Loved one's name: $lovedOneName \n";
+        }
+        $messageBody .= "Client target: $clientTarget \n";
+        $messageBody .= "Client gender: $clientGender \n";
+        if ( $clientFeeling ) {
+          $messageBody .= "Client feeling: $clientFeeling \n";
+        }
+        if ( $lovedOneFeeling ) {
+          $messageBody .= "Loved one's feeling: $lovedOneFeeling \n";
+        }
+        
+        foreach ( $substancesUsed as $substance ) {
+          $messageBody .= "Substance used: $substance \n";
+        }
+        $messageBody .= "Other substances: $otherSubstances \n";
+
+        foreach ( $effects as $effect ) {
+          $messageBody .= "Effect in life: $effect \n";
+        }
+
+        $messageBody .= "Phone number: $phoneNumber \n";
+        $messageBody .= "City: $city \n";
+        $messageBody .= "Email: $email \n";
+        $messageBody .= "Date of birth: $dob \n";
+        $messageBody .= "Have insurance?: $insurance \n";
+        $messageBody .= "Insurance company: $insuranceCompany \n";
+        $messageBody .= "Comments: $comments \n";
+
+        $to = 'tuan@pivotandpilot.com';
+        $message = $messageBody;
+        $subject = 'Message from SoberLife';
+        $header = '';
+
+        wp_mail($to, $subject, $message, $headers);
+
+        if ( wp_mail($to, $subject, $message, $headers) ) {
+          echo '<section class="thank-you">
+                  <p>Thank you for submittin your form, one of us will give you a call soon!</p>
+                  <a href="">Return Home</a>
+                </section>';
+        } else {
+          echo '<div class="thank-you"> Request sent unsuccessfully </div>';
+        }
+      }
+    ?>
+
+    <div class="form__x-button">X</div>
 
     <section class="loading-screen">
       <div></div>
     </section>
 
     <section class="question__your-name">
-      <h1>What is your name?</h1>
-      <div class="inputWrapper">
-        <input id="client-name" type="text" placeholder="First & Last Name">
-        <div class="nextButton">-></div>
+      <div class="content-wrapper">
+        <h1>What is your name?</h1>
+        <div class="inputWrapper">
+          <input id="client-name" type="text" placeholder="First & Last Name">
+          <div class="nextButton">-></div>
+        </div>
       </div>
     </section>
 
     <section class="question__client-target">
-      <h1>Who are you looking to help?</h1>
-      <div name="client-target" data-value="myself">Myself</div>
-      <div name="client-target" data-value="a loved one">A Loved One</div>
+      <div class="content-wrapper">
+        <h1>Who are you looking to help?</h1>
+        <div class="target-wrapper">
+          <div name="client-target" data-value="myself">Myself</div>
+          <div name="client-target" data-value="a loved one">A Loved One</div>
+        </div> 
+        <div>Back</div>
+      </div>
     </section>
 
     <section class="question__loved-one-name">
@@ -82,7 +168,7 @@
         <p>Lost your job, a special someone, your hame, etc.</p>
       </div>
       <div name="effect-life" data-value="physical health">
-        <h3>Phusical Health</h3>
+        <h3>Physical Health</h3>
         <p>Weak body, organ failure, unhealthy eating habits, etc.</p>
       </div>
       <div name="effect-life" data-value="mental health">
@@ -93,14 +179,14 @@
 
     <section class="submit">
       <div>
-        <input name="personal info" class="form__phone-number" type="number" placeHolder="Phone Number">
+        <input name="personal info" class="form__phone-number" type="tel" placeHolder="Phone Number">
         <input name="personal info" class="form__city" type="text" placeHolder="City">
       </div>
 
       <div>
-        <input name="personal info" class="form__email" type="email" placeHolder="Email">
-        <input name="personal info" class="form__state" type="text" placeHolder="State">
-        <input name="personal info" class="form__date" type="date" placeHolder="Date of Birth">
+        <input name="personal-info" class="form__email" type="email" placeHolder="Email">
+        <input name="personal-info" class="form__state" type="text" placeHolder="State">
+        <input name="personal-info" class="form__dob" type="date" placeHolder="Date of Birth">
       </div>
 
       <div>
@@ -115,23 +201,17 @@
 
       <div>
         <p>If unsure please wirte 'unsure'</p>
-        <input type="text" placeHolder="Insurance Company">
+        <input type="text" name="insurance-company" placeHolder="Insurance Company">
       </div>
 
       <div>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-        <button>Submit</button>
+        <textarea name="comments" id="" cols="30" rows="10" placeHolder="Comments"></textarea>
+        <div id="form__submit">Submit</div>
       </div>
 
       <div><- Back</div>
     </section>
-
-    <section class="thank-you">
-      <p>Thank you for submittin your form, one of us will give you a call soon!</p>
-      <button>Return Home</button>
-    </section>
-
-  </form>
+  </div>
 
 
 
@@ -143,11 +223,12 @@
 
 
 
-  <form action="" id="sober-life-hidden-form">
+
+  <form action="" id="sober-life-hidden-form" method="POST" enctype="multipart/form-data">
     <!-- client's name -->
     <div>name</div>
-    <input id="client-name-hidden" name="client-name-hidden" type="text"> 
-    <input id="loved-one-name-hidden" name="client-name-hidden" type="text"> 
+    <input id="client-name-hidden" name="client-name-hidden" type="text" placeHolder="your name"> 
+    <input id="loved-one-name-hidden" name="loved-one-name-hidden" type="text" placeHolder="loved one's name"> 
 
     <!-- client target -->
     <div>client target</div>
@@ -172,22 +253,35 @@
 
     <!-- substances used -->
     <div>substances used</div>
-    <input type="radio" data-name="substances-used-hidden" value="alcohol">
-    <input type="radio" data-name="substances-used-hidden" value="cocaine">
-    <input type="radio" data-name="substances-used-hidden" value="meth">
-    <input type="radio" data-name="substances-used-hidden" value="weed">
-    <input type="radio" data-name="substances-used-hidden" value="heroine">
-    <input type="radio" data-name="substances-used-hidden" value="crack">
-    <input type="radio" data-name="substances-used-hidden" value="ecstasy">
-    <input type="radio" data-name="substances-used-hidden" value="ketamine">
-    <input type="radio" data-name="substances-used-hidden" value="unsure">
-    <input class="other-substances-used-hidden" type="text" data-name="substances-used-hidden">
+    <input type="radio" data-name="substances-used-hidden" name="substances-used-1" value="alcohol">
+    <input type="radio" data-name="substances-used-hidden" name="substances-used-2" value="cocaine">
+    <input type="radio" data-name="substances-used-hidden" name="substances-used-3" value="meth">
+    <input type="radio" data-name="substances-used-hidden" name="substances-used-4" value="weed">
+    <input type="radio" data-name="substances-used-hidden" name="substances-used-5" value="heroine">
+    <input type="radio" data-name="substances-used-hidden" name="substances-used-6" value="crack">
+    <input type="radio" data-name="substances-used-hidden" name="substances-used-7" value="ecstasy">
+    <input type="radio" data-name="substances-used-hidden" name="substances-used-8" value="ketamine">
+    <input type="radio" data-name="substances-used-hidden" name="substances-used-9" value="unsure">
+    <input class="other-substances-used-hidden" name="other-substances-hidden" type="text" data-name="substances-used-hidden" placeHolder="other substances">
+
+    <!-- how drug effect life -->
+    <div>how drug effect life</div>
+    <input type="radio" data-name="effect-life-hidden" name="effect-1" value="career and personal life">
+    <input type="radio" data-name="effect-life-hidden" name="effect-2" value="physical health">
+    <input type="radio" data-name="effect-life-hidden" name="effect-3" value="mental health">
 
     <!-- personal info -->
-    <input class="form__phone-number--hidden" type="number" placeHolder="Phone Number">
-    <input class="form__city--hidden" type="text" placeHolder="City">
-    <input class="form__email--hidden" type="email" placeHolder="Email">
-    <input class="form__state--hidden" type="text" placeHolder="State">
-    <input class="form__date--hidden" type="date" placeHolder="Date of Birth">
+    <div>personal info</div>
+    <input name="phone-number-hidden" class="form__phone-number--hidden" type="tel" placeHolder="Phone Number">
+    <input name="city-hidden" class="form__city--hidden" type="text" placeHolder="City">
+    <input name="email-hidden" class="form__email--hidden" type="email" placeHolder="Email">
+    <input name="state-hidden" class="form__state--hidden" type="text" placeHolder="State">
+    <input name="dob-hidden" class="form__dob--hidden" type="date" placeHolder="Date of Birth">
+    <div>insurance</div>
+    <input type="radio" name="insurance-hidden" value="yes">
+    <input type="radio" name="insurance-hidden" value="no">
+    <input type="text" name="insurance-company-hidden" placeHolder="insurance company">
+    <textarea name="comments-hidden" id="" cols="30" rows="10" placeHolder="comments"></textarea>
+    <button id="form__submit--hidden">submit</button>
   </form>
 </section>

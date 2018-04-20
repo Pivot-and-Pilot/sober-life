a78602e1719092f26307216ac34b508806788162
Mijung Lee <mijung.m@gmail.com>
2018-04-19 17:02:56 -0700

    styled

--- functions.php
+++ functions.php
@@ -136,9 +136,9 @@ function sober_life_scripts() {
 	// wp_enqueue_style( 'sober-life-slick-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css', true );
 	// js files
 
-  // if ( is_home() ) {
+  if ( is_home() ) {
 	  wp_enqueue_script( 'sober-life-front-page-js', get_template_directory_uri() . '/src/scripts/front-page.js', array(), true );
-  // }
+  }
   if ( !is_page('sober-collective') ) {
 	  wp_enqueue_script( 'sober-life-success-stories-js', get_template_directory_uri() . '/src/scripts/success-stories.js', array(), true );
   }

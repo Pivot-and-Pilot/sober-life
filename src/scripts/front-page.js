5f868f78b6129a3ef7fd041b25c296883b4cf1bb
TuanPham303 <phamcongtuan303@gmail.com>
2018-04-30 15:57:24 -0700

    fix red error

--- src/scripts/front-page.js
+++ src/scripts/front-page.js
@@ -10,12 +10,14 @@ jQuery(document).ready(function($){
         }, 1500 * i);
       }
       setInterval(() => {
-        for (let i = 0; i < $('.front-page__header__changing-slogans-wrapper')[0].children.length; i++) {
-          setTimeout(() => {
-            $($('.front-page__header__changing-slogans-wrapper')[0].children).css('opacity','0');
-            $($('.front-page__header__changing-slogans-wrapper')[0].children[i]).css('opacity','1');
-            // $($('.front-page__header__changing-slogans-wrapper')[0].children[1 + i]).css('opacity', '1');
-          }, 1500 * i);
+        if ( $('.front-page__header__changing-slogans-wrapper').length != 0 ) {
+          for (let i = 0; i < $('.front-page__header__changing-slogans-wrapper')[0].children.length; i++) {
+            setTimeout(() => {
+              $($('.front-page__header__changing-slogans-wrapper')[0].children).css('opacity','0');
+              $($('.front-page__header__changing-slogans-wrapper')[0].children[i]).css('opacity','1');
+              // $($('.front-page__header__changing-slogans-wrapper')[0].children[1 + i]).css('opacity', '1');
+            }, 1500 * i);
+          }
         }
       }, ($('.front-page__header__changing-slogans-wrapper')[0].children.length - 1) * 2500)
     })();
@@ -38,21 +40,23 @@ jQuery(document).ready(function($){
       }, 1000);
   
       setInterval(() => {
-        setTimeout(() => {
-          $('.moving-forward__content__arrows > img')[0].src = "./wp-content/themes/sober-life/img/src/arrow--black.svg";
-          $('.moving-forward__content__arrows > img')[1].src = "./wp-content/themes/sober-life/img/src/arrow--white.svg";
-          $('.moving-forward__content__arrows > img')[2].src = "./wp-content/themes/sober-life/img/src/arrow--white.svg";
-        }, 0);
-        setTimeout(() => {
-          $('.moving-forward__content__arrows > img')[0].src = "./wp-content/themes/sober-life/img/src/arrow--white.svg";
-          $('.moving-forward__content__arrows > img')[1].src = "./wp-content/themes/sober-life/img/src/arrow--black.svg";
-          $('.moving-forward__content__arrows > img')[2].src = "./wp-content/themes/sober-life/img/src/arrow--white.svg";
-        }, 500);
-        setTimeout(() => {
-          $('.moving-forward__content__arrows > img')[0].src = "./wp-content/themes/sober-life/img/src/arrow--white.svg";
-          $('.moving-forward__content__arrows > img')[1].src = "./wp-content/themes/sober-life/img/src/arrow--white.svg";
-          $('.moving-forward__content__arrows > img')[2].src = "./wp-content/themes/sober-life/img/src/arrow--black.svg";
-        }, 1000);
+        if ( $('.moving-forward__content__arrows').length != 0 ) {
+          setTimeout(() => {
+            $('.moving-forward__content__arrows > img')[0].src = "./wp-content/themes/sober-life/img/src/arrow--black.svg";
+            $('.moving-forward__content__arrows > img')[1].src = "./wp-content/themes/sober-life/img/src/arrow--white.svg";
+            $('.moving-forward__content__arrows > img')[2].src = "./wp-content/themes/sober-life/img/src/arrow--white.svg";
+          }, 0);
+          setTimeout(() => {
+            $('.moving-forward__content__arrows > img')[0].src = "./wp-content/themes/sober-life/img/src/arrow--white.svg";
+            $('.moving-forward__content__arrows > img')[1].src = "./wp-content/themes/sober-life/img/src/arrow--black.svg";
+            $('.moving-forward__content__arrows > img')[2].src = "./wp-content/themes/sober-life/img/src/arrow--white.svg";
+          }, 500);
+          setTimeout(() => {
+            $('.moving-forward__content__arrows > img')[0].src = "./wp-content/themes/sober-life/img/src/arrow--white.svg";
+            $('.moving-forward__content__arrows > img')[1].src = "./wp-content/themes/sober-life/img/src/arrow--white.svg";
+            $('.moving-forward__content__arrows > img')[2].src = "./wp-content/themes/sober-life/img/src/arrow--black.svg";
+          }, 1000);
+        }
       }, 1800)
     })();
   // }

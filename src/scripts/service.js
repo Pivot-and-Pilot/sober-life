c5c63c8902aa286eb1e64d0094df20e6680a1c51
Mijung Lee <mijung.m@gmail.com>
2018-04-30 16:20:18 -0700

    changed comment to reflect correctly

--- src/scripts/service.js
+++ src/scripts/service.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function($) { 
 
   // Set initial setting of first li item open
   (function setInitialApproachIcons() {
@@ -8,7 +8,7 @@ jQuery(document).ready(function($) {
   // On click, only one li open at one time
   (function clickApproachIcons() {
     $('#services-child > .services-child__content-wrap > .services-child__our-approach > ul > li > .services-child__our-approach-list-title-wrapper').click(function() {
-      // Remove all other '+' signs and close description
+      // Remove all other '-' signs and close description
       let siblings = $(this).parent().siblings();
       $(siblings).each(function() {
         let siblingsDescription =  $(this).children()[1];
@@ -16,7 +16,7 @@ jQuery(document).ready(function($) {
       });
       $(siblings).removeClass('open');
 
-      // Add '+' sign and open description
+      // Add '-' sign and open description
       $(this).parent().addClass('open');
       let description = $(this).parent().children()[1];
       $(description).slideDown();

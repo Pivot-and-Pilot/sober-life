6ad126af38c14e048bc2c184e83c0201f41e6291
Mijung Lee <mijung.m@gmail.com>
2018-05-03 13:24:33 -0700

    style tweaks and added favicon

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/SBL_Favicon-15.png" type="image/x-icon">
 	<?php wp_head(); ?>
 </head>
 
